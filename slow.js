7bcd363d5450bb1b9fb5b20a8cd201548b728f34
Chris Taylor <cntaylor@ca.ibm.com>
2016-05-05 23:11:08 -0400

    ct: Make several more slow calls in sequence.

--- slow.js
+++ slow.js
@@ -6,7 +6,61 @@ function slow (callback) {
     }, 1000);
 }
 
+function slower(callback) {
+    setTimeout(() => {
+        callback();
+    }, 2000);
+}
+
+function slowest(callback) {
+    setTimeout(() => {
+        callback();
+    }, 3000);
+}
+
+function slowesterer(callback) {
+    setTimeout(() => {
+        callback();
+    }, 4000);
+}
+
 slow(function () {
-    console.log("done");
+    console.log(1);
+
+    slow(function () {
+        console.log(2);
+
+        slower(function () {
+            console.log(3);
+
+            slow(function () {
+                console.log(4);
+
+                slowest(function () {
+                    console.log(5);
+
+                    slower(function () {
+                        console.log(6);
+
+                        slowesterer(function () {
+                            console.log(7);
+
+                            slowest(function () {
+                                console.log(8);
+
+                                slow(function () {
+                                    console.log(9);
+
+                                    slower(function () {
+                                        console.log(10);
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
 });
 
